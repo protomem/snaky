8769de5ac7f2b0fa462765b452890a4d6a10a97c
protomem <protomem0709@gmail.com>
2024-05-05 17:59:22 +0300

    map render

--- main.c
+++ main.c
@@ -1,9 +1,65 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define COLS 60
+#define ROWS 30
+
+void cursor_hide();
+void cursor_show();
+void cursor_move_back();
+void render_table();
+
 int main()
 {
-    printf("Snaky!\n");
-    
+    cursor_hide();
+
+    bool quit = false;
+    while (!quit) {
+        render_table();
+
+        cursor_move_back();
+    }
+
+    cursor_show();
+
     return EXIT_SUCCESS;
+}
+
+void cursor_hide()
+{
+    printf("\e[?25l");
+}
+
+void cursor_show()
+{
+    printf("\e[?25h");
+}
+
+void cursor_move_back()
+{
+    printf("\e[%iA", ROWS + 2);
+}
+
+void render_table()
+{
+    // Top border
+    printf("┌");
+    for (size_t i = 0; i < COLS; i++)
+        printf("─");
+    printf("┐\n");
+
+    // Left/Right borders and fields
+    for (size_t i = 0; i < ROWS; i++) {
+        printf("│");
+        for (size_t j = 0; j < COLS; j++)
+            printf("·");
+        printf("│\n");
+    }
+
+    // Bottom border
+    printf("└");
+    for (size_t i = 0; i < COLS; i++)
+        printf("─");
+    printf("┘\n");
 }
